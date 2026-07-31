a0bdc66e28afccc977c4c78d724e7052c76979f5
Raja Kumar <kraj9380286@gmail.com>
2026-07-31 09:55:37 +0530

    Add Graph Adjacency List Traversal

---
+++ Easy/Graph_Adjacency_List_Traversal.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    vector<vector<int>> printGraph(int V, vector<pair<int, int>>& edges) {
+       vector<vector<int>>ans(V);
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i].first;
+            int v=edges[i].second;
+            ans[u].push_back(v);
+            ans[v].push_back(u);
+        }
+        return ans;
+    }
+};
